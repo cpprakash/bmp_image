d2de9d1a16e7822d626520e12d98431d8bf80ad7
Chander Prakash <cprakash@gmx.de>
2024-11-17 20:16:18 +0100

    Added write_bmp_header function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 struct BMP_FILE_HEADER {
-  unsigned char magic1 = 'B';
-  unsigned char magic2 = 'M';
+  const unsigned char magic1 = 'B';
+  const unsigned char magic2 = 'M';
   uint32_t bmp_file_size;
   uint16_t reserved1;
   uint16_t reserved2;
@@ -38,19 +38,41 @@ int main(void) {
 
 void write_bmp_header(void) {
   std::cout << "Writing bmp header to the file" << std::endl;
+
+  BMP_FILE_HEADER bmp_file_header;
+  bmp_file_header.bmp_file_size = 70; // total file size
+  bmp_file_header.reserved1 = 0;
+  bmp_file_header.reserved2 = 0;
+  bmp_file_header.starting_address = 54;
+
   std::ofstream bmp_file;
-  bmp_file.open("demo.bmp");
+  bmp_file.open("demo.bmp", std::ios::binary | std::ios::out);
   if (!bmp_file.is_open()) {
     std::cout << "File could not be opened" << std::endl;
   } else {
     std::cout << "File is opened" << std::endl;
+    bmp_file.write((char *)&bmp_file_header.magic1,
+                   sizeof(bmp_file_header.magic1)); // B
+    bmp_file.write((char *)&bmp_file_header.magic2,
+                   sizeof(bmp_file_header.magic2)); // M
+    bmp_file.write((char *)&bmp_file_header.bmp_file_size,
+                   sizeof(bmp_file_header.bmp_file_size)); // total file size
+    bmp_file.write((char *)&bmp_file_header.reserved1,
+                   sizeof(bmp_file_header.reserved1)); // reserverd, should be 0
+    bmp_file.write((char *)&bmp_file_header.reserved2,
+                   sizeof(bmp_file_header.reserved2)); // reserved, should be 0
+    bmp_file.write(
+        (char *)&bmp_file_header.starting_address,
+        sizeof(bmp_file_header.starting_address)); // starting address of the
+                                                   // bmp color data
   }
+  bmp_file.close();
 }
 
 void write_info_header(void) {
   std::cout << "Writing info header to the file" << std::endl;
   std::ofstream bmp_file;
-  bmp_file.open("demo.bmp");
+  bmp_file.open("demo.bmp", std::ios::app | std::ios::binary);
   if (!bmp_file.is_open()) {
     std::cout << "File could not be opened" << std::endl;
   } else {
@@ -61,7 +83,7 @@ void write_info_header(void) {
 void write_color_data(void) {
   std::cout << "Writing color data to the file" << std::endl;
   std::ofstream bmp_file;
-  bmp_file.open("demo.bmp");
+  bmp_file.open("demo.bmp", std::ios::app | std::ios::binary);
   if (!bmp_file.is_open()) {
     std::cout << "File could not be opened" << std::endl;
   } else {
