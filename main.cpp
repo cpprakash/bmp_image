fd9ca555d50fc6352408fe1e8c4210cddeb91af7
Chander Prakash <cprakash@gmx.de>
2024-12-08 21:44:01 +0100

    Minor changes

--- main.cpp
+++ main.cpp
@@ -7,39 +7,10 @@
 
 #include "Image.hpp"
 
-void handle_choice_one(void) {
-  std::cout << "Please enter the file name to create without extension."
-            << std::endl;
-  std::string file_name;
-  std::cin >> file_name;
-  BmpImage img;
-  file_name += file_name + ".bmp";
-  img.create_empty_image(file_name);
-}
+void handle_choice_one(void);   // Create a 1x1 BMP image file
+void handle_choice_two(void);   // Read a BMP image file
+void handle_choice_three(void); // Create a wXy BMP image file
 
-void handle_choice_two(void) {
-  std::cout << "Please enter a file name to read" << std::endl;
-  std::string file_name;
-  std::cin >> file_name;
-  BmpImage img;
-  img.start_reading_bmp_file(file_name + ".bmp");
-}
-
-void handle_choice_three(void) {
-  std::cout << "Please enter a file name to create" << std::endl;
-  std::string file_name;
-  std::cin >> file_name;
-  std::cout << "Please enter a height of image" << std::endl;
-  uint32_t height;
-  std::cin >> height;
-  std::cout << "Please enter a width of image" << std::endl;
-  uint32_t width;
-  std::cin >> width;
-  file_name += file_name + ".bmp";
-  BmpImage img(file_name, height, width);
-
-  // img.create_empty_image(file_name);
-}
 /**
  * Creates a menu so that the user can select the option
  * for now the options are
@@ -82,4 +53,36 @@ void create_menu(void) {
 int main(void) {
   create_menu();
   return 0;
+}
+
+void handle_choice_one(void) {
+  std::cout << "Please enter the file name to create without extension."
+            << std::endl;
+  std::string file_name;
+  std::cin >> file_name;
+  BmpImage img;
+  file_name += file_name + ".bmp";
+  img.create_empty_image(file_name);
+}
+
+void handle_choice_two(void) {
+  std::cout << "Please enter a file name to read" << std::endl;
+  std::string file_name;
+  std::cin >> file_name;
+  BmpImage img;
+  img.start_reading_bmp_file(file_name + ".bmp");
+}
+
+void handle_choice_three(void) {
+  std::cout << "Please enter a file name to create" << std::endl;
+  std::string file_name;
+  std::cin >> file_name;
+  std::cout << "Please enter a height of image" << std::endl;
+  uint32_t height;
+  std::cin >> height;
+  std::cout << "Please enter a width of image" << std::endl;
+  uint32_t width;
+  std::cin >> width;
+  file_name += file_name + ".bmp";
+  BmpImage img(file_name, height, width);
 }
